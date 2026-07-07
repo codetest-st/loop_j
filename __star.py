b90b519943079b23bf61e484326ee6f27bcc68f0
codetest-st <codetest.st@gmail.com>
2026-07-07 13:42:01 +0530

    my program

---
+++ __star.py
@@ -0,0 +1,15 @@
+'''---*'''
+i=1
+while i<=5:
+    j=1
+    while j<=5-i:
+        print("_",end="")
+        j=j+1
+    k=1
+    while k<=i:
+        print("*",end="")
+        if k!=i:
+            print("_",end="")
+        k=k+1
+    print()
+    i=i+1
